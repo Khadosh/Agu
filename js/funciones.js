13a9f31dabc61f7c82f1af0ed3501e429d03d14c
Joaquín Nader <joaquinnader@yahoo.com.ar>
2012-06-12 23:15:58 -0300

    mirá, ya lo arreglé jaja

--- js/funciones.js
+++ js/funciones.js
@@ -27,18 +27,25 @@ var activarLink = function(){
 var cambiarVideo = function(){
 	$('#MedioCentro li').click(function(){
 		var id = $(this).attr('id');
-		
+		var link;
 		if (id == "vid1"){
-		  $('#LinkVideo').attr("src", "http://www.youtube.com/v/7sf5zG7nHGc?version=3&amp;hl=es_ES");
+			link = "http://www.youtube.com/v/7sf5zG7nHGc?version=3&amp;hl=es_ES";
 		}
 		if (id == "vid2"){
-		  $('#LinkVideo').attr("src", "http://www.youtube.com/v/JV4Mq804Tlg?version=3&amp;hl=es_ES");
+		  link= "http://www.youtube.com/v/JV4Mq804Tlg?version=3&amp;hl=es_ES";
 		}
 		if (id == "vid3"){
-		  $('#LinkVideo').attr("src", "http://www.youtube.com/v/63G_QwlD5OQ?version=3&amp;hl=es_ES");
+		  link = "http://www.youtube.com/v/63G_QwlD5OQ?version=3&amp;hl=es_ES";
 		}
 		if (id == "vid4"){
-		  $('#LinkVideo').attr("src", "http://www.youtube.com/v/tkTPRMGKVew?version=3&amp;hl=es_ES");
+		  link= "http://www.youtube.com/v/tkTPRMGKVew?version=3&amp;hl=es_ES";
 		}
+		
+		$('#Reproductor').html(""
+				+ "<param id=\"movie\" name=\"movie\" value="+ link +"></param>"
+				+ "<param name=\"allowFullScreen\" value=\"true\"></param>"
+				+ "<param name=\"allowscriptaccess\" value=\"always\"></param>"
+				+ "<embed src=" +link+" type=\"application/x-shockwave-flash\" width=\"400\" height=\"280\" allowscriptaccess=\"always\" allowfullscreen=\"true\"></embed>"
+		);
 	});
 };
